cc96955dcc6e6df80f824284e754c0af5c0b2465
HANSAOUI_NACEUREDDINE@emsi-edu.ma <HANSAOUI_NACEUREDDINE@emsi-edu.ma>
2021-08-05 20:32:51 +0100

    Creating project and setting the connection to MongoDB in application.properties file

---
+++ src/main/java/com/amigoscode/springwithmongodb/SpringwithmongodbApplication.java
@@ -0,0 +1,13 @@
+package com.amigoscode.springwithmongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringwithmongodbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringwithmongodbApplication.class, args);
+    }
+
+}
